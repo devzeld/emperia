d3bdaba47e2a5e167e91999dec9faf37f8bc68e5
eldis.izeirovski <zeld.exe.dev@gmail.com>
2025-04-11 13:48:31 +0200

    added connection to the server with Client.java

--- src/main/java/me/zeld/emperia/api/ReignController.java
+++ src/main/java/me/zeld/emperia/api/ReignController.java
@@ -1,26 +1,28 @@
 package me.zeld.emperia.api;
 
+import me.zeld.emperia.reign.network.Client;
 import me.zeld.emperia.reign.service.*;
-import me.zeld.emperia.reign.util.type.*;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
+@RequestMapping(value = "/reign", produces = "application/json")
 public class ReignController {
-    Reign test;
+    Reign reign;
 
+    @GetMapping(value = "{name}/full", produces = "application/json")
+    public Reign getFullReign(@PathVariable String name) {
+        if (reign != null) {
+            System.out.println("Reign found");
+            if (reign.getName().equals(name)) {
+                return reign;
+            }
+            Client.delete(name);
+            reign = null;
+            System.out.println("Reign deleted");
+        }
+        reign = Client.create(name);
+        System.out.println("Reign created");
 
-    @GetMapping("/reign")
-    public Reign getReign() {
-        test = new Reign("test", 1234);
-        Terrain t = Terrain.create(TerrainType.FIELD);
-        t.build(new Building(BuildingType.FARM, 60));
-        test.addTerrain(t);
-
-        return test;
+        return reign;
     }
 }
